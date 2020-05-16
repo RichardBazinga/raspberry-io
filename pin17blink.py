2668e1502cd5943a70c98d61b221318715f340a1
RichardBazinga <richard.vieira.cps@gmail.com>
2020-05-16 02:04:40 -0300

    Testing parameters

--- pin17blink.py
+++ pin17blink.py
@@ -1,7 +1,12 @@
 #!/urs/bin/env python3
 import os
+import sys
 from gpiozero import LED
 from time import sleep
+
+print sys.argv[0] # prints python_script.py
+print sys.argv[1] # prints var1
+print sys.argv[2] # prints var2
 
 led = LED(17)
 os.system("echo '...Running pin17bkink.py'")
