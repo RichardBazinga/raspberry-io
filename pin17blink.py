851a09a9b436b3f047e17599436c27f9eb4496a7
Richard Vieira <richard.vieira@route.local>
2020-05-16 00:54:39 -0300

    Killer function was removed

--- pin17blink.py
+++ pin17blink.py
@@ -4,8 +4,8 @@ from gpiozero import LED
 from time import sleep
 
 led = LED(17)
-os.system("Echo 'killing python'")
-os.system("kill -9 `pgrep python`")
+os.system("...Running pin17bkink.py")
+
 while True:
     led.on()
     sleep(0.1)
--- pin17off.py
+++ pin17off.py
@@ -3,7 +3,7 @@ import os
 from gpiozero import LED
 
 led = LED(17)
-os.system("Echo 'killing python'")
-os.system("kill -9 `pgrep python`")
+os.system("...Running pin17off.py")
+
 while True:
     led.off()
