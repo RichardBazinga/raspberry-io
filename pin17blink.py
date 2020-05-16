dc0e85c70458d645866b3d1f4015bd2d4ec1c86a
RichardBazinga <richard.vieira.cps@gmail.com>
2020-05-16 02:22:03 -0300

    adding arguments

--- pin17blink.py
+++ pin17blink.py
@@ -4,17 +4,13 @@ import sys
 from gpiozero import LED
 from time import sleep
 
-print sys.argv[0] # prints python_script.py
-print sys.argv[1] # prints var1
-print sys.argv[2] # prints var2
-
 led = LED(17)
 os.system("echo '...Running pin17bkink.py'")
 
 while True:
     led.on()
-    sleep(0.1)
+    sleep(sys.argv[1])
     led.off()
-    sleep(0.15)
+    sleep(sys.argv[2])
 
 
---
+++ pythontester.py
@@ -0,0 +1,9 @@
+#!/urs/bin/env python3
+
+# import module sys to get the type of exception
+import sys
+
+try:
+    val
+except NameError:
+    pass  # val does not exist at all
